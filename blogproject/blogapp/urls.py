0e865aeba128e5d2d7d9eb03682294179e3d4b62
Mressence <cordobaguerra19@gmail.com>
2025-04-16 13:35:41 -0500

    solving some errors

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView
-
+from django.contrib.auth import views as auth_views
 app_name = 'blogapp'
 
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
     path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
