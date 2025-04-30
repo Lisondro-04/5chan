7e77091f36f74c595cc2b12cb65c18c1a3aa33d0
Mressence <cordobaguerra19@gmail.com>
2025-04-30 14:40:33 -0500

    added reviews validation

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
 
 # MODELOS
@@ -25,6 +26,22 @@ class Review(models.Model):
     def __str__(self):
         return f"{self.reviewer.username} - {self.blog.title}"
 
+    def clean(self):
+        # Validar que el comentario tenga al menos 10 caracteres
+        if len(self.comment.strip()) < 10:
+            raise ValidationError("El comentario debe tener al menos 10 caracteres.")
+        
+        # Validar que el usuario no deje más de una review por blog
+        if Review.objects.filter(blog=self.blog, reviewer=self.reviewer).exclude(pk=self.pk).exists():
+            raise ValidationError("Ya has dejado una reseña para este blog.")
+
+        # Validar lenguaje inapropiado
+        if "ofensivo" in self.comment.lower():
+            raise ValidationError("Tu comentario contiene lenguaje inapropiado.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Aplica validaciones antes de guardar
+        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
