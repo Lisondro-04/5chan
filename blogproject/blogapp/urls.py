c3b04dd152beb2920ea98e6c9b89034341865d84
Lisondro <josuelisondro2304@hotmail.com>
2025-04-30 19:33:57 -0500

    commit

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView
 from django.contrib.auth import views as auth_views
-app_name = 'blogapp'
 
+app_name = 'blogapp'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
@@ -11,5 +11,5 @@ urlpatterns = [
     path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
     path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # ✅ logout funcional
 ]
