5058cc36b40ea46a93f713c105c53f81bf0eb6e8
Lisondro <josuelisondro2304@hotmail.com>
2025-04-30 19:31:15 -0500

    texto

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
+from ckeditor.fields import RichTextField
 
 
 # MODELOS
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
