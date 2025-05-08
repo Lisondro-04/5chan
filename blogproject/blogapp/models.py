6d93178c7884524c4721853b9ab05bf727d5d05c
Lisondro <josuelisondro2304@hotmail.com>
2025-05-07 19:02:46 -0500

    ui improvements

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -21,7 +21,7 @@ class Review(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='reviews')
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    comment = models.CharField()
+    comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
